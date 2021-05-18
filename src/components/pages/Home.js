186a90faa76ad889c3c1cbf3fb646b0f9c613845
FilMel <melnicf96@gmail.com>
2021-05-18 12:39:26 +0200

    before react-booking form

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import '../../App.css';
-import '../Body'
-import Body from '../Body';
-import HeroSection from '../HeroSection';
-
+import React from "react";
+import "../../App.css";
+import "../Body";
+import Body from "../Body";
+import HeroSection from "../HeroSection";
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <Body/>
-
+      <Body />
     </>
   );
 }
