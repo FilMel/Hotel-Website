7b08fe1f28d41b509ea6ae142707e7531701cabb
FilMel <melnicf96@gmail.com>
2021-05-18 13:55:46 +0200

    Added a form function

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
-import Cyber from "./videos/cyber.mp4"
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import Cyber from "./videos/cyber.mp4";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
+    <div className="hero-container">
       <video autoPlay loop muted>
         <source src={Cyber} type="video/mp4" />
-        </video>
+      </video>
       <h1>In tutta autonomia!</h1>
       <p>Il futuro è adesso</p>
-      <div className='hero-btns'>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           BOOK NOW
         </Button>
-        
       </div>
     </div>
   );
--- src/components/pages/Book.js
+++ src/components/pages/Book.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../App.css";
+import Form from "../BookingForm"
 
 export default function Book() {
-  return <h1 className="book">LIKE & SUBSCRIBE</h1>;
+  return <h1 className="book"><Form></Form> </h1>;
 }
